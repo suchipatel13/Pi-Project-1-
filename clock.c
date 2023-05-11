c1ab090c291fa02cad836013ddfb1416d44d104f
suchipatel13 <127153219+suchipatel13@users.noreply.github.com>
2023-05-11 16:51:23 -0400

    Update clock.c

--- clock.c
+++ clock.c
@@ -1,39 +1,89 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <time.h>
 #include <unistd.h>
 #include "FBCurses.h"
 
-void convertTime(int num1, colorcode_t oncolor, colorcode_t offcolor,int column){
-        int count = 7;
-        while(num1 !=0){
-                if (num1 %2 ==0){
-                        setPixel(count,column,offcolor);
-
+char*binary(int numbers,int count){
+        char backwards[8];
+        char *list = malloc(count);
+        int counter = 0;
+        while(counter!=count){
+                if(numbers%2!=0){
+                        backwards[counter]=0;
+                        numbers=numbers/2;
+                        counter++;
+                }
+                else{
+                        backwards[counter]=0;
+                        numbers = numbers/2;
+                        counter++;
+                }
         }
-        else{
-                setPixel(count,column,oncolor);
+        int i;
+        int j=0;
+        for(i =counter-1;i>=0;i--){
+                list[j]=backwards[i];
+                j++;
         }
-        num1 = num1/2;
-        count --;
-
-        }
-
-        for (int i = count; i> = 0; i--){
-                setPixel(i,column,offcolor);
-        }
+        return list;
 }
+
 
 void showTime(colorcode_t oncolor,colorcode_t offcolor){
-        time_t latestime;
-        struct tm * timeinfo;
-        time(&latestime);
-        timeinfo = localtime (&latestime);
-
-        binaryTime(timeinfor->tm_hour, oncolor,offcolor,1)
-         binaryTime(timeinfor->tm_min, oncolor,offcolor,3);
-         binaryTime(timeinfor->tm_sec, oncolor,offcolor,5);
+        for(int y=0;y<6;y++){
+                if(y==1){
+                        times_t hours(time(NULL)/60/60-4)%24;
+                        char *list=binary(hours,8);
+                        if(list){
+                        for(int x=0;x<8;x++){
+                                if(list[x]==0){
+                                        setPixel(x,y,offcolor);
+                                }
+                                else{
+                                        setPixel(x,y,oncolor);
+                                }
+                        }
+                        free(list);
+                        }
+                }
+                if(y==3){
+                        times_t seconds=time(NULL)/60%60;
+                        char*list = binary(minutes,8);
+                        if(list){
+                                for(int x=0;x<8;x++){
+                                                if(list[x]==0){
+                                                        setPixel(x,y,offcolor);
+                                                }
+                                                else{
+                                                        setPixel(x,y,oncolor);
+                                                }
+                                }
+                                free(list);
+                                   }
+                }
+                if(y==5){
+                        times_t seconds = time(NULL)%60;
+                        char*list = binary(seconds,8);
+                        if(list){
+                                for(int x=0;x<8;x++){
+                                        if(list[x]==0){
+                                                setPixel(x,y,offcolor);
+                                        }
+                                        else{
+                                                setPixel(x,y,oncolor);
+                                        }
+                                }
+                        free(list);
+                        }
+                          }
+                if(y%2==0){
+                        for(int x=0;x<8;x++){
+                                setPixel(x,y,offcolor);
+                        }
+                }
+        }
 }
-
 int main(){
         initScreen();
         //feel free to change the color scheme to your liking.
@@ -43,8 +93,9 @@ int main(){
         clearScreen(background);
         while (!kbhit()){
                 showTime(oncolor,offcolor);
-                sleep(0.5);  //sleep for 1 second
+                sleep(.5);
         }
         cleanupScreen();
 }
-   
+          
+                               
